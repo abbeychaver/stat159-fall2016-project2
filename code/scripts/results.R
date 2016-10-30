21fa808238150e95e76776d25e35ecb4e4e2959b
Tina Huang <thuang@berkeley.edu>
2016-10-29 23:40:02 -0700

    report: still working on plotting the coefficients

--- code/scripts/results.R
+++ code/scripts/results.R
@@ -2,6 +2,13 @@ load("../../data/Ridge.Rdata")
 load("../../data/lasso.Rdata")
 load("../../data/pcr.Rdata")
 load("../../data/plsr.Rdata")
+scaled_data <- read.csv("data/datasets/scaled-credit.csv")[, -1]
+
+library(ggplot2)
+
+ols = lm(Balance~Income+Limit+Rating+Cards+Age+Education+GenderFemale+StudentYes+MarriedYes+EthnicityAsian+EthnicityCaucasian, data=scaled_data)
+ols_coeff = ols$coefficients[-1]
+ols_mse = mean(ols$residuals^2)
 
 ridge_mse = mse
 pcr_mse = test_mses[12]
@@ -12,11 +19,13 @@ pls_mse
 ridge_coeff = coef(fcv)[2:12,]
 pcr_coeff = best_coefs
 
-pls_coeff
-
 vars = names(pls_coeff)
-coeff_mat = matrix(c(unname(ridge_coeff), unname(lasso_coeff), unname(pcr_coeff), unname(pls_coeff)), ncol=11)
+coeff_mat = t(matrix(c(unname(ols_coeff), unname(ridge_coeff), unname(lasso_coeff), unname(pcr_coeff), unname(pls_coeff)), ncol=11))
 colnames(coeff_mat) = vars
-rownames(coeff_mat) = c("Ridge", "Lasso", "PCR", "PLSR")
+rownames(coeff_mat) = c("OLS", "Ridge", "Lasso", "PCR", "PLSR")
 coeff_table = as.table(coeff_mat)
 
+coeff_plot = ggplot(as.data.frame(coeff_mat)) + geom_bar()
+ggplot(unname(ridge_coeff), aes(x=)) + geom_bar()
+
+a = as.data.frame(ridge_coeff)
