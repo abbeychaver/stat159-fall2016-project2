69b126362e2b5856cb299a39e002270f4c7c156d
Tina Huang <thuang@berkeley.edu>
2016-10-30 17:34:59 -0700

    two different plots for model coefficients and mse matrix

--- code/scripts/results.R
+++ code/scripts/results.R
@@ -6,38 +6,29 @@ scaled_data <- read.csv("data/datasets/scaled-credit.csv")[, -1]
 
 library(ggplot2)
 
+# ordinary least squares regression
 ols = lm(Balance~Income+Limit+Rating+Cards+Age+Education+GenderFemale+StudentYes+MarriedYes+EthnicityAsian+EthnicityCaucasian, data=scaled_data)
 ols_coeff = ols$coefficients[-1]
 ols_mse = mean(ols$residuals^2)
 
 ridge_mse = mse
 pcr_mse = test_mses[12]
-
-lasso_mse
-pls_mse
-
 ridge_coeff = coef(fcv)[2:12,]
 pcr_coeff = best_coefs
 
+# turn off scientific notation
 options("scipen" = 100, "digits" = 1)
+
+# create coefficient matrix for all the models
 vars = names(pls_coeff)
 coeff_mat = t(matrix(c(unname(ols_coeff), unname(ridge_coeff), unname(lasso_coeff), unname(pcr_coeff), unname(pls_coeff)), ncol=11))
 rownames(coeff_mat) = vars
 colnames(coeff_mat) = c("OLS", "Ridge", "Lasso", "PCR", "PLSR")
+
+# Create table from the matrix
 coeff_table = as.table(coeff_mat)
 
-
-ggplot(as.data.frame(coeff_mat), aes(x=gender, y = Freq, fill=fraud)) + geom_bar(stat="identity")
-
-
-coeff_plot = ggplot(as.data.frame(coeff_mat)) + geom_bar()
-ggplot(unname(ridge_coeff), aes(x=)) + geom_bar()
-
-a = as.data.frame(ridge_coeff)
-b = factor(as.data.frame(coeff_mat)$OLS, labels=vars)
-c = as.data.frame(coeff_mat)$OLS
-plot(c ~ factor(vars))
-
+# Create data frame for the coefficients and models for plotting
 coeff_df = data.frame(rep(vars, 5), c(unname(ols_coeff), unname(ridge_coeff), unname(lasso_coeff), unname(pcr_coeff), unname(pls_coeff)), c(rep("OLS", 11), rep("Ridge", 11), rep("Lasso", 11), rep("PCR", 11), rep("PLSR", 11)))
 colnames(coeff_df) = c("vars", "coeff", "model")
 
@@ -45,6 +36,13 @@ colnames(coeff_df) = c("vars", "coeff", "model")
 coeff_df$vars = factor(coeff_df$vars, levels=coeff_df$vars)
 coeff_df$model = factor(coeff_df$model, levels=coeff_df$model)
 
-ggplot(coeff_df, aes(vars, coeff)) + geom_bar(stat="identity") + facet_grid( ~ model)
+# 5 plots separated by model
+coeff_plot_separate = ggplot(coeff_df, aes(vars, coeff)) + geom_bar(stat="identity") + facet_wrap( ~ model) + xlab("Predictor") + ylab("Coefficient Value") + ggtitle("Comparison of Coefficient Values for Different Models")
 
-melt(coeff_df)
+# single multibar plot
+coeff_plot = ggplot(coeff_df, aes(vars, coeff)) + geom_bar(aes(fill = model), position = "dodge", stat="identity") + xlab("Predictor") + ylab("Coefficient Value") + ggtitle("Comparison of Coefficient Values for Different Models")
+
+options("scipen" = 0, "digits" = 7)
+mse_mat = t(matrix(c(ols_mse, ridge_mse, lasso_mse, pcr_mse, pls_mse)))
+rownames(mse_mat) = vars
+colnames(mse_mat) = c("OLS", "Ridge", "Lasso", "PCR", "PLSR")
